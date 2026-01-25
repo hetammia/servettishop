51b23a1bae4180f4f282f9c441e5e6d4ef8a4930
hetammia <tammia.emilia@gmail.com>
2026-01-25 19:57:25 +0200

    initialised project

---
+++ servettishop/src/App.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Welcome to the shop</h1>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Hope you found what you were looking for</p>
+    </div>
+  )
+}
+
+export default App
