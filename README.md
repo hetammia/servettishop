ea1bee9bcfb74ee557e0fa6138960be8fdd86dd3
hetammia <tammia.emilia@gmail.com>
2026-01-30 15:19:14 +0200

    created skeleton for the page

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# servettishop
+# Welcome to ServettiShop!
+
+> Planning a party and want to make it more special? Look no further! We at ServettiShop rent napkins in multitude of qualities and colours.
+
+This is a practice app for a simple shopfront of a napkin rental. The focus is more on practicing React rather than creating an examplary shop UI.
+
+
--- servettishop/src/App.jsx
+++ servettishop/src/App.jsx
@@ -1,8 +1,130 @@
+import { useState } from 'react'
+
 const Header = () => {
   return (
     <div>
-      <h1>Welcome to the shop</h1>
+      <h1>ServettiShop</h1>
+      <p>Serving the best Servettis</p>
     </div>
+  )
+}
+const SelectionForm = () => {
+  const [selectedMaterial, setMaterial] = useState('cotton')
+  const [selectedColour, setColour] = useState('white')
+  const [selectedQuantity, setQuantity] = useState('10')
+
+  const handleMaterialChange = (event) => {
+    setMaterial(event.target.value)
+  }
+  const handleColourChange = (event) => {
+    setColour(event.target.value)
+  }
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    alert(`You are purchasing ${selectedQuantity} ${selectedColour} ${selectedMaterial} napkins`)
+    event.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <h2>Select your Servetti</h2>
+        <p>Both our cotton and linen napkins come in a variety of colours. <br/>
+          See below which one meets your fancy.</p>
+
+        <h3>Napkin Material</h3>
+        <label>
+          <input
+            type="radio"
+            name="material"
+            value="cotton"
+            checked={selectedMaterial === 'cotton'}
+            onChange={handleMaterialChange}
+          /> Cotton
+        </label>
+        <label>
+          <input 
+            type='radio' 
+            name='material'
+            value='linen'
+            checked={selectedMaterial === 'linen'}
+            onChange={handleMaterialChange}
+          /> Linen
+        </label>
+
+        <h3>Colour</h3>
+        <label>
+          <input 
+            type='radio'
+            name='colour'
+            value='white'
+            checked={selectedColour === 'white'}
+            onChange={handleColourChange}
+          /> White
+        </label>
+        <label>
+          <input 
+            type='radio'
+            name='colour'
+            value='ivory'
+            checked={selectedColour === 'ivory'}
+            onChange={handleColourChange}
+          /> Ivory
+        </label>
+        <label>
+          <input 
+            type='radio'
+            name='colour'
+            value='petrol'
+            checked={selectedColour === 'petrol'}
+            onChange={handleColourChange}
+          /> Petrol
+        </label>
+
+        <h3>Quantity</h3>
+        <label>
+          <input 
+            type='radio'
+            name='quantity'
+            value='10'
+            checked={selectedQuantity === '10'}
+            onChange={handleQuantityChange}
+          /> 10
+        </label>
+        <label>
+          <input 
+            type='radio'
+            name='quantity'
+            value='30'
+            checked={selectedQuantity === '30'}
+            onChange={handleQuantityChange}
+          /> 30
+        </label>
+        <label>
+          <input 
+            type='radio'
+            name='quantity'
+            value='50'
+            checked={selectedQuantity === '50'}
+            onChange={handleQuantityChange}
+          /> 50
+        </label>
+        <label>
+          <input 
+            type='radio'
+            name='quantity'
+            value='100'
+            checked={selectedQuantity === '100'}
+            onChange={handleQuantityChange}
+          /> 100
+        </label>
+        <br/>
+        <button type='submit'>Purchase</button>
+      </div>
+    </form>
   )
 }
 
@@ -10,6 +132,7 @@ const App = () => {
   return (
     <div>
       <Header />
+      <SelectionForm />
       <p>Hope you found what you were looking for</p>
     </div>
   )
